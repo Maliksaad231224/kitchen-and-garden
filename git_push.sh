89575f7bc79bd9469ec40102e8b450b7195f58b7
unknown <maliksaadamed17@gmail.com>
2025-11-09 11:52:19 +0500

    Update files

---
+++ git_push.sh
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "Update files"
+git push -u origin master
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,21 @@ const config = {
       screens: {
         'xs': '380px',
       },
+      colors: {
+        // Override Tailwind's red palette so all red-* classes use the requested hex
+        red: {
+          50:  '#B91C1C',
+          100: '#B91C1C',
+          200: '#B91C1C',
+          300: '#B91C1C',
+          400: '#B91C1C',
+          500: '#B91C1C',
+          600: '#B91C1C',
+          700: '#B91C1C',
+          800: '#B91C1C',
+          900: '#B91C1C',
+        },
+      },
     },
   },
   plugins: [],
